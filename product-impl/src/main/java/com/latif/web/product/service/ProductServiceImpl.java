5ef8adcf0599f59906a9ee84e73c1d6e85e6f7d5
latifis <latifirfansyah@gmail.com>
2021-12-06 11:48:36 +0700

    add get one product service

--- product-impl/src/main/java/com/latif/web/product/service/ProductServiceImpl.java
+++ product-impl/src/main/java/com/latif/web/product/service/ProductServiceImpl.java
@@ -2,11 +2,13 @@ package com.latif.web.product.service;
 
 import com.latif.web.product.dto.input.ProductInput;
 import com.latif.web.product.dto.output.ProductOutput;
+import com.latif.web.product.model.Product;
 import com.latif.web.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -23,7 +25,19 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public ProductOutput getOne(Long id) {
-        return null;
+        //optional tuh kayak nullable
+        Optional<Product> product = repository.findById(id);
+        if (product.isEmpty()){
+            return null;
+        }
+        Product temp = product.get();
+        return ProductOutput
+                .builder()
+                .id(temp.getId())
+                .name(temp.getName())
+                .description(temp.getDescription())
+                .price(temp.getPrice())
+                .build();
     }
 
     @Override
