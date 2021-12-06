2d8210da22920592c166117fe1889380a67296bc
latifis <latifirfansyah@gmail.com>
2021-12-06 13:00:31 +0700

    add mapping for product controller

--- product-impl/src/main/java/com/latif/web/product/controller/ProductController.java
+++ product-impl/src/main/java/com/latif/web/product/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.latif.web.product.controller;
 
 import com.latif.web.product.dto.input.ProductInput;
 import com.latif.web.product.dto.output.ProductOutput;
+import com.latif.web.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,20 +11,30 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/product")
-public class ProductController {
+@RequiredArgsConstructor
+public class ProductController {          //pakai implements ProductService untuk create  get add and all
+    private final ProductService productService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductOutput> getOne(@PathVariable Long id){ //path variabel tuh buat nandain id sebagai path variabel
-        return ResponseEntity.ok(ProductOutput.builder().build());
+    public ResponseEntity<ProductOutput> getOne(@PathVariable Long id) {  //jangan lupa add path variabel krn ada getmapping id
+        ProductOutput output = productService.getOne(id);
+        return ResponseEntity.ok(output);
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductOutput>> getAll(){
-        return ResponseEntity.ok(null);
+    public List<ProductOutput> getAll() {
+        return null;
     }
 
-    @PostMapping("/{id}")
-    public ResponseEntity<ProductOutput> getOne(ProductInput productInput){ //path variabel tuh buat nandain id sebagai path variabel
-        return ResponseEntity.ok(ProductOutput.builder().build());
+    @PostMapping
+    public ResponseEntity<Boolean> addOne(@RequestBody ProductInput input) {   //janganlupa request body karena harus req body
+        productService.addOne(input);
+        return ResponseEntity.ok(Boolean.TRUE);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteOne(@PathVariable Long id) {
+        return ResponseEntity.ok(Boolean.TRUE);
+    }
+    //ResponEntity untuk memberikan respon entity 200 atau 400 dsb
 }
