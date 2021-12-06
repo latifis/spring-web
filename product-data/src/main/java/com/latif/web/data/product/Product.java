8a20fd564444921c5bf8f022c10019e085b6c4b1
latifis <latifirfansyah@gmail.com>
2021-12-06 17:39:34 +0700

    add module product data

---
+++ product-data/src/main/java/com/latif/web/data/product/Product.java
@@ -0,0 +1,40 @@
+package com.latif.web.data.product;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "bni_product")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 255)
+    private String name;
+
+    @Column //kalau mau ganti nama tinggal => column(name = "desc")
+    private String description;
+
+    @Column
+    private Double price;
+
+    @Column(name = "createAt")
+    @CreationTimestamp
+    private Date createAt;
+
+    @Column(name = "updateAt")
+    @UpdateTimestamp
+    private Date updateAt;
+}
